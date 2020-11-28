d8361966a0618fb191b5344d21dc2d43bd13c114
yoddi dahsyat <yoddi.dahsyat@gmail.com>
2020-11-28 18:52:05 +0700

    Class-Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 // CLASS COMPONENT
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Content from './Components/Content';
 
-// class App extends Component{
-//   render(){
-//     return <p>Hello World!</p>
-//   }
-// }
-
-// export default App;
-
-
-// FUNCTIONAL COMPONENT
-import React from 'react';
-
-const App = ()=> {
-  const name = 'yoddi';
-  return (
-    <div>
-      <p>Hello, {name}!</p>
-      <p>this is your App.</p>
-    </div>
-  )
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <Header/>
+        <Content/>
+      </div>
+    )
+  }
 }
 
 export default App;
+
+
+// FUNCTIONAL COMPONENT
+// import React from 'react';
+
+// const App = ()=> {
+//   const name = 'yoddi';
+//   return (
+//     <div>
+//       <p>Hello, {name}!</p>
+//       <p>this is your App.</p>
+//     </div>
+//   )
+// }
+
+// export default App;
---
+++ src/Components/Content.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import "../App.css";
+
+class Content extends Component {
+    render() {
+        return (
+            <div className="App-content">
+                <h1>This is Content</h1>
+            </div>
+        )
+    }
+}
+
+export default Content;
