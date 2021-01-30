6ff9c92104acb7a46da4e959b91338ca7aadff24
yoddi dahsyat <yoddi.dahsyat@gmail.com>
2021-01-30 20:39:53 +0700

    uploadURL

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const baseURL = "https://waysbucks-api.herokuapp.com/api/v1";
-export const uploadURL = "http://res.cloudinary.com/ddocknxiq/image/";
+export const uploadURL = "http://res.cloudinary.com/ddocknxiq/";
 
 export const API = axios.create({
     baseURL
